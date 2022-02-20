9d069c00ebace3108685d869839eeb4335862ae2
noKeyShin <chmich321@gmail.com>
2022-02-20 20:31:12 +0100

    Site_V3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("bonjour");
+let listeCourse = document.getElementById("listeCourse");
+let planning = document.getElementById("planning");
+let ingredients = document.getElementById("ingredients");
+
+  listeCourse.addEventListener("click", () => {
+  document.getElementById('rubrique').className = "hidden"
+  document.getElementById('ingredients').className = "visible"
+  listeCourse.style.backgroundColor = '#c3f3b779';
+  planning.style.backgroundColor = '#62755B';
+  
+  })
+
+  planning.addEventListener("click", () => {
+    document.getElementById('rubrique').className = "visible"
+    document.getElementById('ingredients').className = "hidden"
+    listeCourse.style.backgroundColor = '#62755B';
+    planning.style.backgroundColor = '#c3f3b779';
+    })
